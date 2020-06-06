3ca002e63340ce3d127e3e2885fdf1500bf6e72e
Parveez Fathima <Parveez_Fathima@swissre.com>
2020-06-06 11:24:36 +0530

    initial commit

---
+++ DemoForms/app/StudentForm/sfService.js
@@ -0,0 +1,12 @@
+﻿studentFormsApp.factory('sfService',
+    function () {
+        return {
+            student: {
+                fullName: "Kishor Bikram Oli",
+                objective: "To become a great software engineer",
+                department: "Physics",
+                hobbies: "Gaming",
+                gender: "Male"
+            }
+        }
+    });
